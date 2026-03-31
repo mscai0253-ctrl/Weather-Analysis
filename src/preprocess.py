b24f9b5638781da47d0bd6a76817d12490d07a6d
Avdhut Bane <mscai0253@gmail.com>
2026-03-31 21:33:34 +0530

    Update preprocess.py

--- src/preprocess.py
+++ src/preprocess.py
@@ -3,19 +3,19 @@ import pandas as pd
 def load_and_clean_data():
     df = pd.read_csv("data/weather.csv")
 
-    # Clean column names
+    
     df.columns = df.columns.str.strip()
 
-    # Rename columns to standard format
+   
     df.rename(columns={
         "Date": "date",
         "Temperature_C": "temperature"
     }, inplace=True)
 
-    # Convert date
+   
     df['date'] = pd.to_datetime(df['date'])
 
-    # Drop missing values
+    
     df = df.dropna()
 
     # Feature engineering
